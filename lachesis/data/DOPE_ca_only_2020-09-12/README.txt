8040dc7a5383aca34dfa01b60185cf9547f12847
Apollinaire Roubert <apo.roubert@gmail.com>
2020-09-12 14:35:48 +0200

    Created dope_tri_ca.py which extracts all line in a dope.par file which involve 2 CAs. Results of running the program were put in a new repository in data and appropriately labeled

---
+++ lachesis/data/DOPE_ca_only_2020-09-12/README.txt
@@ -0,0 +1,1 @@
+File obtained by running dope.par through dope_tri_ca.py
---
+++ lachesis/src/dope_tri_ca.py
@@ -0,0 +1,36 @@
+"""
+This script will scour a DOPE potential file, find all lines that deal with only Alpha carbon interactions for both residues, extract them, then write a new .par file with only those lines.
+
+Takes as argument a DOPE potential file under the .par format
+Returns a .par file named "dope_ca_only.par" with only the CA-CA lines
+
+Usage:
+
+    python3 dope_tri_ca.py argument1
+
+"""
+
+__authors__ = ("Apollinaire Roubert")
+__contact__ = ("apo.roubert@gmail.com")
+__version__ = "1.0.0"
+__date__ = "2020/09/12"
+
+import sys
+
+def extract_lines(dope_file):
+    dope_ca_only = []
+    with open(dope_file, "r") as filin:
+        lines = filin.readlines()
+        for line in lines:
+            if line.split()[1] == "CA" and line.split()[3] == "CA":
+                dope_ca_only.append(line)
+    return dope_ca_only
+
+def write_new_dope(new_dope_lines):
+    with open("dope_ca_only.par", "w") as filout:
+        for line in new_dope_lines:
+            filout.write(line)
+
+if __name__ == "__main__":
+    new_dope_lines = extract_lines(sys.argv[1])
+    write_new_dope(new_dope_lines)
