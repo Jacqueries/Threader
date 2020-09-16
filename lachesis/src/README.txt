300cddfdb69dfe1707204b749282e4517ef4a57c
Apollinaire Roubert <apo.roubert@gmail.com>
2020-09-16 22:49:45 +0200

    Added the final report and the supplementary pages (annexe) in separate .pdf files in doc. Added a readme in src to indicate that the equivalent to a main is high_level.py. Removed a placeholder .txt file. Changed some lines in high_level.py.

---
+++ lachesis/src/README.txt
@@ -0,0 +1,4 @@
+The equivalent to a main.py here is high_level.py
+
+To understand how to call it, type :
+        python3 high_level.py help
--- lachesis/src/high_level.py
+++ lachesis/src/high_level.py
@@ -6,11 +6,6 @@ Usage:
     
     python3 high_level.py pdb_file.pdb seq_file.fasta dope_file.par
 
-
-NOTICE : CHANGE THE GAP PENALTIES, THE DIFFERENCES ARE MAJOR
-
-
-
 """
 
 __authors__ = ("Apollinaire Roubert", "Ilyas Granguillaume")
@@ -25,7 +20,7 @@ def high_level(low_scores, query, template_length):
     Args :
         -low_scores: Dict containing final scores for each fixed position
 	-query: sequence to thread
-	-template_length: number of position   
+	-template_length: number of position
     """
     h_mat = prog_dynam_matrix(list(range(template_length)), query)
     h_mat.create_content()
@@ -42,4 +37,3 @@ if __name__ == '__main__':
         query = readTargSeq(sys.argv[2])
         mat_score = generate_low(mat_col, query, sys.argv[3])
         high_level(mat_score, query, len(template))
-        
